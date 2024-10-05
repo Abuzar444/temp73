91cc4377765913df5a8d2f2625c5f9d11a30a55d
= <=>
2024-10-05 16:19:27 +0500

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
-import logo from '@/assets/logo.svg';
-import Image from 'next/image';
-import landingImage from '@/assets/main.svg';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <header className="max-w-6xl mx-auto px-4 sm:px-8 py-6">
-        <Image src={logo} alt="logo" />
+      <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6'>
+        {/* <Image src={logo} alt="logo" /> */}
+        Hello
       </header>
-      <section className="max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
+      <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
         <div>
-          <h1 className="capitalize text-4xl md:text-7xl font-bold">
-            job <span className="text-primary">tracking</span> app
+          <h1 className='capitalize text-4xl md:text-7xl font-bold'>
+            job <span className='text-primary'>tracking</span> app
           </h1>
-          <p className="leading-loose max-w-md mt-4">
+          <p className='leading-loose max-w-md mt-4'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
             excepturi quia, soluta enim veritatis ullam sunt debitis cumque modi
             provident, eius commodi fugiat deserunt magnam natus corporis, nisi
             quidem! Alias.
           </p>
-          <Button asChild className="mt-4">
-            <Link href="add-job">Get Started</Link>
+          <Button asChild className='mt-4'>
+            <Link href='add-job'>Get Started</Link>
           </Button>
         </div>
-        <Image src={landingImage} alt="landing" className="hidden lg:block" />
+        {/* <Image src={landingImage} alt="landing" className="hidden lg:block" /> */}
+        Hello
       </section>
     </main>
   );
