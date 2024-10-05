67dea2c8333dcfa9440e122f93be4357e967d5dc
= <=>
2024-10-05 16:23:32 +0500

    commit

--- app/(dashboard)/jobs/[id]/page.tsx
+++ app/(dashboard)/jobs/[id]/page.tsx
@@ -1,26 +1,24 @@
 import EditJobForm from "@/components/EditJobForm";
 import { getSingleJobAction } from "@/utils/actions";
+
 import {
+  dehydrate,
   HydrationBoundary,
   QueryClient,
-  dehydrate,
-  useQuery,
 } from "@tanstack/react-query";
-import React from "react";
 
-const SingleJobPage = async ({ params }: { params: { id: string } }) => {
-  const { id } = params;
+async function JobDetailPage({ params }: { params: { id: string } }) {
   const queryClient = new QueryClient();
+
   await queryClient.prefetchQuery({
-    queryKey: ["job", id],
-    queryFn: () => getSingleJobAction(id),
+    queryKey: ["job", params.id],
+    queryFn: () => getSingleJobAction(params.id),
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <EditJobForm jobId={id} />
+      <EditJobForm jobId={params.id} />
     </HydrationBoundary>
   );
-};
-
-export default SingleJobPage;
+}
+export default JobDetailPage;
